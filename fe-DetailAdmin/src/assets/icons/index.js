87f2b8984b7b9c404b4fb1c752bba7fc54f12bb2
AlvinAndrian <alvin.andrian.tif20@polban.ac.id>
2021-10-17 17:06:49 +0700

    Pembuatan page Detail Admin

---
+++ fe-DetailAdmin/src/assets/icons/index.js
@@ -0,0 +1,30 @@
+import IconFeed from './feed.svg';
+import IconFeedActive from './feedA.svg';
+import IconSearch from './search.svg'
+import IconSearchActive from './searchA.svg';
+import IconToolbar from './toolbar.svg';
+import IconSafety from './safety.svg';
+import IconSafetyActive from './safetyA.svg';
+import IconProfil from './profil.svg';
+import IconProfilActive from './profilA.svg';
+import IconAdd from './add.svg';
+import IconArrowLeft from './arrowLeft.svg';
+import IconDelete from './delete.svg';
+import IconEdit from './edit.svg';
+
+export {
+    IconFeed,
+    IconSearch,
+    IconToolbar,
+    IconSafety,
+    IconProfil,
+    IconProfilActive,
+    IconSafetyActive,
+    IconSearchActive,
+    IconFeedActive,
+    IconDelete,
+    IconArrowLeft,
+    IconEdit,
+    IconAdd
+}
+
---
+++ fe-DetailAdmin/src/pages/index.js
@@ -0,0 +1,7 @@
+import Feed from './feed';
+import Profil from './profil';
+import Safety from './safety';
+import Toolbar from './toolbar';
+import Search from './search';
+
+export { Feed, Profil, Safety, Toolbar, Search }
